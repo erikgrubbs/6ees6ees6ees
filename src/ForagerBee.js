bc8def063fd682ac22cb72db0db2312da0778193
Erik Grubbs <erikgrubbs@gmail.com>
2019-06-04 15:53:50 -0700

    completes es6 instantiation for all bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor() {
+    super();
+    this.age  = 10;
+    this.job = 'find pollen';
+    this.treasureChest = [];
+    this.canFly = true;
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
